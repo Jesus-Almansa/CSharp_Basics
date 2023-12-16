09aa4813bf485d5bc0fcf361d8d8dd7aa0de7b41
Jesús Almansa <jtafernandez@gmail.com>
2023-12-16 14:14:41 +0100

    v.1

---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,49 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        float num1;
+        float num2;
+        float result;
+        string answer;
+
+        Console.WriteLine("Hello World! Welcome to the calculator program \n");
+        Console.WriteLine("Please enter your first number");
+
+        num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Your first number is " + num1 + "\n");
+
+        Console.WriteLine("Please enter your second number");
+
+        num2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Your second number is " + num2 + "\n");
+
+        Console.WriteLine("What type of operation would you like to do?");
+        Console.WriteLine(" 'a' for addition \n 's' for subtraction \n 'm' for multiplication \n 'd' for division");
+
+        answer = Console.ReadLine();
+        if (answer == "a")
+        {
+            result = num1 + num2;
+            
+        }
+        else if (answer == "s")
+        {
+            result = num1 - num2;
+        }
+        else if (answer == "m")
+        {
+            result = num1 * num2;
+        }
+        else if (answer == "d")
+        {
+            result = num1 / num2;
+        }
+        else
+        {
+            Console.WriteLine("Invalid operation selected");
+            return; // Optionally, you might want to exit the program if an invalid operation is selected.
+        }
+        Console.WriteLine("The result is " + result);
+    }
+}
---
+++ Dice Game/Dice Game/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+internal class Dice_Game
+{
+    private static void Main(string[] args)
+    {
+        int playerRandomNum;
+        int enemyRandomNum;
+        Random random = new Random();
+
+        int playerPoints = 0;
+        int enemyPoints = 0;
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Press any button to role the dice");
+            Console.ReadKey();
+
+            playerRandomNum = random.Next(1, 7);
+            Console.WriteLine("You rolled a " + playerRandomNum);
+
+            Console.WriteLine("...");
+            System.Threading.Thread.Sleep(500);
+
+            enemyRandomNum = random.Next(1, 7);
+            Console.WriteLine("AI rolled a " + enemyRandomNum);
+
+            if (playerRandomNum > enemyRandomNum)
+            {
+                playerPoints++;
+                Console.WriteLine("Player WINS this round \n");
+            }
+            else if (playerRandomNum < enemyRandomNum)
+            {
+                enemyPoints++;
+                Console.WriteLine("AI WINS this round \n");
+            }
+            else
+            {
+                Console.WriteLine("DRAW \n");
+            }
+            Console.WriteLine("The score is now - Player : " + playerPoints + ". AI : " + enemyPoints + ".");
+        }
+        if (playerPoints > enemyPoints)
+        {
+            Console.WriteLine("Player WINS THE GAME \n");
+            Console.WriteLine("Total score " + playerPoints);
+        }
+        else if (playerPoints < enemyPoints)
+        {
+            Console.WriteLine("AI WINS THE GAME \n");
+            Console.WriteLine("Total score " + enemyPoints);
+        }
+        else
+        {
+            Console.WriteLine("THE GAME ENDS AS DRAW");
+        }
+        
+    }
+}
